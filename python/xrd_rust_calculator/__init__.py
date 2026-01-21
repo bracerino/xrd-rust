63d57a05b7bed4e05f31d2b9915887078fe13c4f
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2026-01-22 00:31:45 +0100

    Update __init__.py

--- python/xrd_rust_calculator/__init__.py
+++ python/xrd_rust_calculator/__init__.py
@@ -2,9 +2,8 @@
 
 from .xrd_calculator_rust import (
     XRDCalculatorRust,
-    create_xrd_calculator,
     WAVELENGTHS,
 )
 
 __version__ = "0.1.0"
-__all__ = ["XRDCalculatorRust", "create_xrd_calculator", "WAVELENGTHS"]
+__all__ = ["XRDCalculatorRust", "WAVELENGTHS"]
